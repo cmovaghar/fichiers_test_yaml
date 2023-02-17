604f9e1fb808bdefc928b86b1370e58b56562e4a
cmovaghar <120220542+cmovaghar@users.noreply.github.com>
2023-02-16 19:26:44 -0500

    Add files via upload

---
+++ prog_1/exec.kt
@@ -0,0 +1,11 @@
+fun main() {
+	// +VISIBLE
+	// Les variables doivent avoir la même valeur. À faire
+	// +TODO
+    var haha: Int = 0
+    var hihi: Int = 1
+
+	// -TODO
+	// -VISIBLE
+    if (haha == hihi) println(" ")
+}
